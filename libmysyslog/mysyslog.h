6816f1ec2bae626a9dfd2d3c7f085012e5cbddf9
Neon-Invader <91779051+Neon-Invader@users.noreply.github.com>
2024-11-25 21:33:15 +0300

    Create mysyslog.h

---
+++ libmysyslog/mysyslog.h
@@ -0,0 +1,13 @@
+#ifndef MYSYSLOG_H
+#define MYSYSLOG_H
+
+// Уровни журналирования
+#define LEVEL_DEBUG 0
+#define LEVEL_INFO  1
+#define LEVEL_WARN  2
+#define LEVEL_ERROR 3
+#define LEVEL_CRITICAL 4
+
+int mysyslog(const char* msg, int level, int driver, int format, const char* path);
+
+#endif // MYSYSLOG_H
